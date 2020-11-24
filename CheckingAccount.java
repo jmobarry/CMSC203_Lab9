e9bca8a557fc17f60a64b20cee26e3e8d5f8cd2c
John Mobarry <70714969+jmobarry@users.noreply.github.com>
2020-11-24 11:05:05 -0500

    Add files via upload

---
+++ CheckingAccount.java
@@ -0,0 +1,15 @@
+
+public class CheckingAccount extends BankAccount {
+
+	private static final double FEE = .15;
+	
+	public CheckingAccount(String name, double amount) {
+		super(name,amount);
+		super.setAccountNumber(super.getAccountNumber() + "-10");
+	}
+	
+	public boolean withdraw(double amount) {
+		return super.withdraw(amount + FEE);
+	}
+	
+}
